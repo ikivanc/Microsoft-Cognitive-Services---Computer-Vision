66434bcde5bf6299f226995febc55f5bc9985915
Ibrahim KIVANC <ikivanc@hotmail.com>
2018-01-05 17:51:19 +0300

    Computer Vision API First push
    Microsoft Cognitive Services, Computer Vision API First Push

---
+++ CS ComputerVision/CS Computer Vision/ComputerVision.cs
@@ -0,0 +1,178 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Computer_Vision
+{
+    public class ComputerVision
+    {
+        [JsonProperty(PropertyName = "categories")]
+        public Category[] Categories { get; set; }
+
+        [JsonProperty(PropertyName = "tags")]
+        public Tag[] Tags { get; set; }
+
+        [JsonProperty(PropertyName = "description")]
+        public Description Description { get; set; }
+
+        [JsonProperty(PropertyName = "requestId")]
+        public string RequestId { get; set; }
+
+        [JsonProperty(PropertyName = "metadata")]
+        public Metadata Metadata { get; set; }
+
+        [JsonProperty(PropertyName = "faces")]
+        public Face[] Faces { get; set; }
+
+        [JsonProperty(PropertyName = "color")]
+        public Color Color { get; set; }
+
+        [JsonProperty(PropertyName = "imageType")]
+        public Imagetype ImageType { get; set; }
+
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+    }
+
+    public class Description
+    {
+        [JsonProperty(PropertyName = "tags")]
+        public string[] Tags { get; set; }
+
+        [JsonProperty(PropertyName = "captions")]
+        public Caption[] Captions { get; set; }
+    }
+
+    public class Caption
+    {
+        [JsonProperty(PropertyName = "text")]
+        public string Text { get; set; }
+
+        [JsonProperty(PropertyName = "confidence")]
+        public float Confidence { get; set; }
+    }
+
+    public class Metadata
+    {
+        [JsonProperty(PropertyName = "width")]
+        public int Width { get; set; }
+
+        [JsonProperty(PropertyName = "height")]
+        public int Height { get; set; }
+
+        [JsonProperty(PropertyName = "format")]
+        public string Format { get; set; }
+    }
+
+    public class Color
+    {
+        [JsonProperty(PropertyName = "dominantColorForeground")]
+        public string DominantColorForeground { get; set; }
+
+        [JsonProperty(PropertyName = "dominantColorBackground")]
+        public string DominantColorBackground { get; set; }
+
+        [JsonProperty(PropertyName = "dominantColors")]
+        public string[] DominantColors { get; set; }
+
+        [JsonProperty(PropertyName = "accentColor")]
+        public string AccentColor { get; set; }
+
+        [JsonProperty(PropertyName = "isBWImg")]
+        public bool IsBWImg { get; set; }
+    }
+
+    public class Imagetype
+    {
+        [JsonProperty(PropertyName = "clipArtType")]
+        public int ClipArtType { get; set; }
+
+        [JsonProperty(PropertyName = "lineDrawingType")]
+        public int LineDrawingType { get; set; }
+    }
+
+    public class Category
+    {
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "score")]
+        public float Score { get; set; }
+
+        [JsonProperty(PropertyName = "detail")]
+        public Detail Detail { get; set; }
+    }
+
+    public class Detail
+    {
+        [JsonProperty(PropertyName = "celebrities")]
+        public Celebrity[] Celebrities { get; set; }
+    }
+
+    public class Celebrity
+    {
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "faceRectangle")]
+        public Facerectangle FaceRectangle { get; set; }
+
+        [JsonProperty(PropertyName = "confidence")]
+        public float Confidence { get; set; }
+    }
+
+    public class Facerectangle
+    {
+        [JsonProperty(PropertyName = "left")]
+        public int Left { get; set; }
+
+        [JsonProperty(PropertyName = "top")]
+        public int Top { get; set; }
+
+        [JsonProperty(PropertyName = "width")]
+        public int Width { get; set; }
+
+        [JsonProperty(PropertyName = "height")]
+        public int Height { get; set; }
+    }
+
+    public class Tag
+    {
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "confidence")]
+        public float Confidence { get; set; }
+    }
+
+    public class Face
+    {
+        [JsonProperty(PropertyName = "age")]
+        public int Age { get; set; }
+
+        [JsonProperty(PropertyName = "gender")]
+        public string Gender { get; set; }
+
+        [JsonProperty(PropertyName = "faceRectangle")]
+        public Facerectangle1 FaceRectangle { get; set; }
+    }
+
+    public class Facerectangle1
+    {
+        [JsonProperty(PropertyName = "left")]
+        public int Left { get; set; }
+
+        [JsonProperty(PropertyName = "top")]
+        public int Top { get; set; }
+
+        [JsonProperty(PropertyName = "width")]
+        public int Width { get; set; }
+
+        [JsonProperty(PropertyName = "height")]
+        public int Height { get; set; }
+    }
+
+}
---
+++ CS ComputerVision/CS Computer Vision/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace CS_Computer_Vision
+{
+    class Program
+    {
+        //computer vision için gerekli olan key ve request'in yapılacağı uriBase
+        const string subscriptionKey_Vision = "YOUR KEY";
+        const string uriBase_Vision = "https://westeurope.api.cognitive.microsoft.com/vision/v1.0";
+
+        static void Main()
+        {
+
+            Console.Write("Enter image URL: ");
+            var imageURL = Console.ReadLine();
+
+            //// Computer Vision - Image Tag Örneği
+            ComputerVisionRequest(imageURL);
+
+            //Computer Vision Altında OCR Request Örneği
+            OCRRequest(imageURL);
+
+            Console.ReadLine();
+        }
+
+        static async void ComputerVisionRequest(string imageUrl)
+        {
+            var client = new HttpClient();
+
+            // Request headers
+            // Face, Tags, Categories, ImageType, Color, Celebrity, Landmark detaylarının alınacağı sorgu aşağıdaki gibidir
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey_Vision);
+            string uri = uriBase_Vision + "/analyze?visualFeatures=Faces,Tags,categories,Description,ImageType,Color&details=Celebrities,Landmarks";
+
+
+            HttpResponseMessage response;
+
+            // Request body
+            byte[] byteData = Encoding.UTF8.GetBytes("{\"Url\": \"" + imageUrl + "\"}");
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                response = await client.PostAsync(uri, content);
+
+                //Json Deserialization
+                string json = response.Content.ReadAsStringAsync().Result;
+                json = json.TrimStart(new char[] { '[' }).TrimEnd(new char[] { ']' });
+                ComputerVision photoVision = JsonConvert.DeserializeObject<ComputerVision>(json);
+
+                List<string> myTags = new List<string>();
+                Console.WriteLine("---\n\nCategory Detayları");
+                foreach (Category pCategory in photoVision.Categories)
+                {
+                    Console.WriteLine(pCategory.Name + "-" + pCategory.Score + "-" + pCategory.Detail);
+                }
+
+                Console.WriteLine("---\n\nPhoto Tag Detayları");
+                foreach (Tag pTag in photoVision.Tags)
+                {
+                    Console.WriteLine(pTag.Name + "-" + pTag.Confidence);
+                    myTags.Add(pTag.Name);
+                }
+
+                Console.WriteLine("---\n\nDescription Tag Detayları");
+                foreach (string pTag in photoVision.Description.Tags)
+                {
+                    Console.WriteLine(pTag);
+                    myTags.Add(pTag);
+                }
+
+                List<string> myDecription = new List<string>();
+                foreach (Caption pCaption in photoVision.Description.Captions)
+                {
+                    Console.WriteLine(pCaption.Text + " - " + pCaption.Confidence);
+                    myDecription.Add(pCaption.Text);
+                }
+
+
+                foreach (Face pface in photoVision.Faces)
+                {
+                    Console.WriteLine("---\n\nDescription Tag Detayları");
+                    Console.WriteLine("Age: " + pface.Age);
+                    Console.WriteLine("Gender: " + pface.Gender);
+                }
+
+                Console.WriteLine("---\n\nColor Tag Detayları");
+                Console.WriteLine("dominantColorForeground: " + photoVision.Color.DominantColorForeground);
+                Console.WriteLine("dominantColorBackground: " + photoVision.Color.DominantColorBackground);
+                Console.WriteLine("accentColor: " + photoVision.Color.AccentColor);
+                Console.WriteLine("isBWImg: " + photoVision.Color.IsBWImg);
+
+                Console.WriteLine("---\n\nDominantColors Detayları");
+                foreach (string dColors in photoVision.Color.DominantColors)
+                {
+                    Console.WriteLine(dColors);
+                }
+
+            }
+        }
+
+        static async void OCRRequest(string imageUrl)
+        {
+
+            var client = new HttpClient();
+
+            // Request headers
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey_Vision);
+            string uri = uriBase_Vision + "/ocr?language=tr&detectOrientation=true";
+            HttpResponseMessage response;
+
+            //Console.Write("Enter image URL: ");
+            //var imageUrl = Console.ReadLine();
+
+            // Request body
+            byte[] byteData = Encoding.UTF8.GetBytes("{\"Url\": \"" + imageUrl + "\"}");
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                try
+                {
+                    content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                    response = await client.PostAsync(uri, content);
+
+                    //JSON Deserialization
+                    string json = response.Content.ReadAsStringAsync().Result;
+                    json = json.TrimStart(new char[] { '[' }).TrimEnd(new char[] { ']' });
+                    OCRVision ocrVision = JsonConvert.DeserializeObject<OCRVision>(json);
+
+                    Console.WriteLine("---\n\nOCR Detayları");
+                    //OCR'dan elde edilen sonuçların ekrana yazılması
+                    foreach (Region ereg in ocrVision.Regions)
+                    {
+                        foreach (Line sline in ereg.Lines)
+                        {
+                            foreach (Word sword in sline.Words)
+                            {
+                                Console.Write(sword.Text + "\n");
+                            }
+                        }
+                    }
+                }
+                catch
+                {
+                    Console.Write("OCR couldn't find");
+                }
+            }
+        }
+
+    }
+}
